6f10ce1bfdb38c683c05bb3b0f1408da5fc73bdd
millennium-faulken <justin.faulkenberry@gmail.com>
2022-05-09 15:58:21 -0500

    added event listeners to nav for scrolling

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,11 +1,11 @@
 import "./landing.css";
 import Nav from "./nav";
-import pic from "../static/ResumePicCircle.png"
+import pic from "../static/ResumePicCircle.png";
 
 function Landing() {
   return (
     <div className="App">
-      <header>
+      <header className="navbar">
         <Nav />
       </header>
       <main>
@@ -13,9 +13,18 @@ function Landing() {
           <div className="landing-info">
             <h1>Hey there, my name is</h1>
             <h2 className="name">Justin Faulkenberry</h2>
-            <h3>I'm a Software Engineer</h3>
+            <hr></hr>
+            <p align="right">
+              I'm a driven full-stack <span className="job-title">software engineer</span>, currently searching for
+              a full-time position. I'm dedicated to expanding knowledge and taking
+              on challenging problems. Highly motivated, enthusiastic, and eager
+              to learn new technologies and methods.
+            </p>
           </div>
-          <img src={pic} alt="This is me!"/>
+          <img src={pic} alt="This is me!" />
+        </section>
+        <section>
+          
         </section>
       </main>
     </div>
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,10 +1,35 @@
+import React, { useState, useEffect, useCallback } from "react";
 import { Link } from "react-router-dom";
 import resume from "../static/resume.pdf";
 import "./landing.css";
 
 function Nav() {
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  const navbarStyles = {
+    position: "fixed",
+    width: "100%",
+    transition: "top .5s",
+  };
+
+  const handleScroll = useCallback(() => {
+    const currentScrollPos = window.pageYOffset;
+    setVisible(
+      (prevScrollPos > currentScrollPos &&
+        prevScrollPos - currentScrollPos > 100) ||
+        currentScrollPos < 250
+    );
+    setPrevScrollPos(currentScrollPos);
+  }, [prevScrollPos]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [prevScrollPos, visible, handleScroll]);
+
   return (
-    <nav>
+    <nav style={{ ...navbarStyles, top: visible ? "0" : "-60px" }}>
       <div className="logo">
         <h3>justinFAULKENBERRY</h3>
       </div>
