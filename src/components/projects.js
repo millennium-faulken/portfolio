2b5dfc0951332784c4c6c440120a56fcad2c4746
millennium-faulken <justin.faulkenberry@gmail.com>
2022-05-26 11:40:45 -0500

    working on projects page

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,8 +1,5 @@
-import urgently from "../static/urgent.png";
-import bubble from "../static/bubble.PNG";
-import messages from "../static/messages.png";
-import quake from "../static/quake.JPG";
 import { BiMessageRoundedError } from "react-icons/bi";
+import { FaGithubSquare, FaExternalLinkSquareAlt } from "react-icons/fa";
 import "./projects.css";
 
 export default function Projects() {
@@ -10,12 +7,10 @@ export default function Projects() {
     <main className="projects">
       <section className="projectSection">
         <h1 className="projectsTitle">Some things I'm working on</h1>
-        <div className="divProject">
-          <div className="quake" align="left">
+        <div className="projectsList">
+          {/* <div className="projectItem">
             <h2>Quake</h2>
-            <p>
-              Utilizes the USGS API to search for recent earthquakes.
-            </p>
+            <p>Utilizes the USGS API to search for recent earthquakes.</p>
             <ul>
               <li>jQuery</li>
               <li>Php</li>
@@ -24,11 +19,7 @@ export default function Projects() {
               <li>Ajax</li>
             </ul>
           </div>
-          <img src={quake} alt="messages" />
-        </div>
-        <div className="divProject">
-          <img src={bubble} alt="bubble" />
-          <div className="bubble" align="right">
+          <div className="projectItem">
             <h2>
               bubble
               <BiMessageRoundedError />
@@ -37,15 +28,13 @@ export default function Projects() {
               A real-time web messaging app with active/inactive user list, and
               google sign-in or email/password authentication.
             </p>
-            <ul dir="rtl">
+            <ul>
               <li>React</li>
               <li>Firebase</li>
               <li>CSS</li>
             </ul>
           </div>
-        </div>
-        <div className="divProject">
-          <div className="urgent" align="left">
+          <div className="projectItem">
             <h2>Urgent!</h2>
             <p>
               A to-do list app with user sign-in,authentication,
@@ -56,33 +45,56 @@ export default function Projects() {
               <li>Firebase</li>
               <li>CSS</li>
             </ul>
-          </div>
-          <img src={urgently} alt="messages" />
-        </div>
-        <div className="divProject">
-          <img src={messages} alt="messages" />
-          <div className="messages" align="right">
-            <h2>Messages to no one...</h2>
+          </div> */}
+          <div className="projectItem">
+            <h1 className="messages">
+              Messages to no one...{"    "}
+              <a
+                href="https://messagetheworld.netlify.app/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaExternalLinkSquareAlt />
+              </a>
+            </h1>
             <p>
-              An app to leave messages to the world using a MERN stack,
-              create(with bad word filter) and read messages.
+              A web app to leave messages to the world using a MERN stack,
+              create (with bad word filter) and read messages.
             </p>
-            <ul dir="rtl">
-              <li>MondgoDb</li>
-              <li>Express.js</li>
-              <li>React.js</li>
-              <li>Node.js</li>
-              <li>Redux</li>
-              <li>Mongoose</li>
-              <li>Axios</li>
-              <li>Sass</li>
-              <li>Heroku</li>
-            </ul>
+            <div className="tech">
+              <ul>
+                <p>Front-End</p>
+                <li>React.js</li>
+                <li>Redux</li>
+                <li>Sass</li>
+                <li>Netlify</li>
+                <a
+                  href="https://github.com/millennium-faulken/notes"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaGithubSquare />
+                </a>
+              </ul>
+              <ul>
+                <p>Back-End</p>
+                <li>Node.js</li>
+                <li>Express.js</li>
+                <li>MondgoDb</li>
+                <li>Mongoose</li>
+                <li>Heroku</li>
+                <a
+                  href="https://github.com/millennium-faulken/notes-backend"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaGithubSquare />
+                </a>
+              </ul>
+            </div>
           </div>
         </div>
       </section>
-      {/* <img src={urgently} className="urgent" alt="urgent" />
-            <img src={messages} className="urgent" alt="urgent" /> */}
     </main>
   );
 }
