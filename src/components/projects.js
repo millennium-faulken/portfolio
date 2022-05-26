08ebaa165c032ee37bec852a559cac0685b74e73
millennium-faulken <justin.faulkenberry@gmail.com>
2022-05-26 12:15:20 -0500

    project page restructure and responsive

--- src/components/projects.js
+++ src/components/projects.js
@@ -6,46 +6,8 @@ export default function Projects() {
   return (
     <main className="projects">
       <section className="projectSection">
-        <h1 className="projectsTitle">Some things I'm working on</h1>
+        <h1 className="projectsTitle">Check out some things I've built</h1>
         <div className="projectsList">
-          {/* <div className="projectItem">
-            <h2>Quake</h2>
-            <p>Utilizes the USGS API to search for recent earthquakes.</p>
-            <ul>
-              <li>jQuery</li>
-              <li>Php</li>
-              <li>HTML</li>
-              <li>CSS</li>
-              <li>Ajax</li>
-            </ul>
-          </div>
-          <div className="projectItem">
-            <h2>
-              bubble
-              <BiMessageRoundedError />
-            </h2>
-            <p>
-              A real-time web messaging app with active/inactive user list, and
-              google sign-in or email/password authentication.
-            </p>
-            <ul>
-              <li>React</li>
-              <li>Firebase</li>
-              <li>CSS</li>
-            </ul>
-          </div>
-          <div className="projectItem">
-            <h2>Urgent!</h2>
-            <p>
-              A to-do list app with user sign-in,authentication,
-              <br /> and CRUD functionality
-            </p>
-            <ul>
-              <li>React</li>
-              <li>Firebase</li>
-              <li>CSS</li>
-            </ul>
-          </div> */}
           <div className="projectItem">
             <h1 className="messages">
               Messages to no one...{"    "}
@@ -61,6 +23,7 @@ export default function Projects() {
               A web app to leave messages to the world using a MERN stack,
               create (with bad word filter) and read messages.
             </p>
+            <h3>Tech Used:</h3>
             <div className="tech">
               <ul>
                 <p>Front-End</p>
@@ -85,6 +48,123 @@ export default function Projects() {
                 <li>Heroku</li>
                 <a
                   href="https://github.com/millennium-faulken/notes-backend"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaGithubSquare />
+                </a>
+              </ul>
+            </div>
+          </div>
+          <div className="projectItem">
+            <h1 className="bubble">
+              bubble
+              <BiMessageRoundedError />
+              {"    "}
+              <a
+                href="https://bubbleitup.netlify.app/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaExternalLinkSquareAlt />
+              </a>
+            </h1>
+            <p>
+              A real-time web messaging app with active/inactive user list, and
+              google sign-in or email/password authentication.
+            </p>
+            <h3>Tech Used:</h3>
+            <div className="tech">
+              <ul>
+                <li>React</li>
+                <li>Firebase</li>
+                <li>CSS</li>
+                <a
+                  href="https://github.com/millennium-faulken/messenger"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaGithubSquare />
+                </a>
+              </ul>
+            </div>
+          </div>
+          <div className="projectItem">
+            <h1 className="quake">
+              Quake{"    "}
+              <a
+                href="https://messagetheworld.netlify.app/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaExternalLinkSquareAlt />
+              </a>
+            </h1>
+            <p>
+              A web site the utilizes the USGS API to show the user the most
+              recent earthquakes in the last month of a specified magnitude or
+              greater
+            </p>
+            <h3>Tech Used:</h3>
+            <div className="tech">
+              <ul>
+                <li>jQuery</li>
+                <li>Php</li>
+                <li>Ajax</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <a
+                  href="https://github.com/millennium-faulken/quake"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaGithubSquare />
+                </a>
+              </ul>
+            </div>
+          </div>
+          <div className="projectItem">
+            <h1 className="winCalc">Windows Calculator Clone</h1>
+            <p>A clone of the Windows Calculator</p>
+            <h3>Tech Used:</h3>
+            <div className="tech">
+              <ul>
+                <li>C#</li>
+                <li>.Net</li>
+                <li>Visual Studio 2022</li>
+                <a
+                  href="https://github.com/millennium-faulken/windows-calculator-clone"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaGithubSquare />
+                </a>
+              </ul>
+            </div>
+          </div>
+          <div className="projectItem">
+            <h1 className="urgent">
+              Urgent!{"    "}
+              <a
+                href="https://urgently.netlify.app"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaExternalLinkSquareAlt />
+              </a>
+            </h1>
+            <p>
+              A to-do list app with user sign-in, authentication, and CRUD
+              functionality.
+            </p>
+            <h3>Tech Used:</h3>
+            <div className="tech">
+              <ul>
+                <li>React</li>
+                <li>Firebase</li>
+                <li>CSS</li>
+                <a
+                  href="https://github.com/millennium-faulken/urgent"
                   target="_blank"
                   rel="noreferrer"
                 >
